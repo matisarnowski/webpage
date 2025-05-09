7c1f6c2202b021712816b2460d4838258d88b359
Mateusz Sarnowski <matisarnowski88@gmail.com>
2025-05-09 18:15:58 +0200

    webpage

--- app-witcher.py
+++ app-witcher.py
@@ -1,7 +1,3 @@
-"""Module PIL providing functions for opening the image. 
-Module os providing functions for opening path to files. 
-Module streamlit is basic module in this code."""
-import os
 from PIL import Image
 import requests
 import streamlit as st
@@ -11,53 +7,26 @@ from streamlit_lottie import st_lottie
 st.set_page_config(page_title="Moja stronka", page_icon=":open_book:", layout="wide")
 
 
-def load_lottieurl(url):  # sourcery skip: assign-if-exp, reintroduce-else
-    """**load_lottieurl(url)**
-
-    Loads and returns the JSON data from a given URL.
-
-    Args:
-        url (str): The URL to load the JSON data from.
-
-    Returns:
-        dict or None: The JSON data loaded from the URL, or None if the request fails.
-
-    Examples:
-        ```python
-        data =
-        load_lottieurl("https://lottie.host/5627c703-4224-48ab-a721-e650f35c3c0f/FrnjXWGhia.json")
-        ```"""
+def load_lottieurl(url):
     r = requests.get(url, timeout=10)
-    return None if r.status_code != 200 else r.json()
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 
 # Use local CSS
 def local_css(file_name):
-    """ "**local_css(file_name)**
-
-    Loads and applies local CSS styles to the Streamlit app.
-
-    Args:
-        file_name (str): The name of the CSS file to load.
-
-    Returns:
-        None
-
-    Examples:
-        ```python
-        local_css("styles/style.css")
-        ```"""
-    with open(file_name, encoding="utf-8") as f:
+    with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-local_css(os.path.join("styles", "style.css"))
+local_css("styles/style.css")
 
 # ---- Load Assets ----
 lottie_coding = load_lottieurl(
     "https://lottie.host/5627c703-4224-48ab-a721-e650f35c3c0f/FrnjXWGhia.json"
 )
-image_code = Image.open(os.path.join("images", "moja_stronka_1.jpg"))
+image_code = Image.open("images/moja_stronka_1.jpg")
 
 # ---- Header Section ----
 st.subheader("Cześć jestem Mateusz :wave:")
@@ -80,12 +49,8 @@ with st.container():
         st.header("Na codzień: ")
         st.write(
             """
-                 - Zazwyczaj po prostu uczę się programować. 
-                 Piszę proste programiki w języku programowania Python, 
-                 lub proste strony Internetowe z użyciem JavaScript, CSS i HTML.
-                 - Poza tym sporo do niedawna siedziałem nad matematyką po prostu musiałem to zrobić. 
-                 Było to wymagane na studiach. Ale nie sprawiło mi to trudności. 
-                 Już od dawna lubię tą dziedzinę nauki.
+                 - Zazwyczaj po prostu uczę się programować. Piszę proste programiki w języku programowania Python, lub proste strony Internetowe z użyciem JavaScript, CSS i HTML.
+                 - Poza tym sporo do niedawna siedziałem nad matematyką po prostu musiałem to zrobić. Było to wymagane na studiach. Ale nie sprawiło mi to trudności. Już od dawna lubię tą dziedzinę nauki.
                  - W ogóle to bardzo lubię się uczyć.
                  """
         )
@@ -107,13 +72,9 @@ with text_column:
     st.subheader("Oto projekt tejże stronki Internetowej oraz moje CV...")
     st.write(
         """
-             Z Netlify nauczyłem się korzystać podczas krótkiego kursu, 
-             na którym tworzyłem stronkę ze swoim CV.
+             Z Netlify nauczyłem się korzystać podczas krótkiego kursu, na którym tworzyłem stronkę ze swoim CV.
              Na codzień po prostu wrzucam coś tylko na Facebooka.
-             No i oczywiście zamieszczam na GitHub. 
-             Ale z nowym Visual Studio Code, 
-             który jest programem open source, 
-             to wszystko jest zautomatyzowane.
+             No i oczywiście zamieszczam na GitHub. Ale z nowym Visual Studio Code, który jest programem open source, to wszystko jest zautomatyzowane.
              """
     )
     st.write("* Oto link do CV z Netlify.")
@@ -136,7 +97,7 @@ with st.container():
     st.write("##")
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    CONTACT_FORM = """
+    contact_form = """
     <form action="https://formsubmit.co/matsarnow@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Twoje imię" required>
@@ -147,6 +108,6 @@ with st.container():
     """
     left_column, right_column = st.columns(2)
     with left_column:
-        st.markdown(CONTACT_FORM, unsafe_allow_html=True)
+        st.markdown(contact_form, unsafe_allow_html=True)
     with right_column:
         st.empty()
